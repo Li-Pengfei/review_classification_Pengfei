85622235982a82820399a8750ead8b5b06250003
IOT-HP9293\dwang015 <wdzethan2010@gmail.com>
2017-12-24 13:20:13 +0800

    confusion matrix plot

--- API/data_helpers.py
+++ API/data_helpers.py
@@ -5,11 +5,9 @@ import itertools
 from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-
-def copy_words(A, B):
-    A = A + ' '
-    return A * B
-
+def copy_words(A,B):
+    A = A+' '
+    return A*B
 
 def clean_str(string):
     """
@@ -23,7 +21,6 @@ def clean_str(string):
     string = re.sub(r"\'re", " \'re", string)
     string = re.sub(r"\'d", " \'d", string)
     string = re.sub(r"\'ll", " \'ll", string)
-    string = re.sub(r"\'m", " \'m", string)
     string = re.sub(r",", " , ", string)
     string = re.sub(r"!", " ! ", string)
     string = re.sub(r"\(", " \( ", string)
@@ -61,7 +58,7 @@ def pad_sentences(sentences, padding_word="<PAD/>"):
         new_sentence = sentence + [padding_word] * num_padding
         padded_sentences.append(new_sentence)
         sentence_length.append(len(sentence))
-    return [sequence_length, padded_sentences, sentence_length]
+    return [sequence_length, padded_sentences,sentence_length]
 
 
 def build_vocab(sentences):
@@ -86,18 +83,6 @@ def build_input_data(sentences, vocabulary):
     return x
 
 
-def split_num(sent):
-    # split number followed by word character
-    sent_new = []
-    for word in sent:
-        match = re.match(r"([0-9]+)([a-zA-Z]+)", word, re.I)
-        if match:
-            sent_new.append(match.group(1))
-            sent_new.append(match.group(2))
-        else:
-            sent_new.append(word)
-    return sent_new
-
 
 def load_data(corpus):
     """
@@ -109,46 +94,41 @@ def load_data(corpus):
     x_text = [s.strip() for s in corpus]
     sentences = [clean_str(sent) for sent in x_text]
     sentences = [s.split(" ") for s in sentences]
-    sentences = [split_num(s) for s in sentences]
 
-    sequence_length, sentences_padded, sen_length = pad_sentences(sentences)
+    sequence_length, sentences_padded,sen_length = pad_sentences(sentences)
     vocabulary, vocabulary_inv = build_vocab(sentences_padded)
-    # x = build_input_data(sentences_padded, vocabulary)
-    x = np.array(sentences_padded)
+    x = build_input_data(sentences_padded, vocabulary)
     return [x, sequence_length, vocabulary, vocabulary_inv]
 
-
-def make_bow(corpus, bow_file):
+def make_bow(corpus,bow_file):
     if os.path.exists(bow_file):
-        a = cPickle.load(open(bowpath, "rb"))
-        c_vocab, c_vector = a[0], a[1]
+        a = cPickle.load(open(bowpath,"rb"))
+        c_vocab,c_vector = a[0], a[1]
     else:
-        c_dict = TfidfVectorizer(lowercase=False, stop_words='english', min_df=2)
+        c_dict = TfidfVectorizer(lowercase=False,stop_words='english',min_df=2)
         c_vector = c_dict.fit_transform(corpus)
         c_vocab = c_dict.get_feature_names()
         cPickle.dump([c_vocab, c_vector], open(bow_file, "wb"))
 
-    return c_vocab, c_vector
+    return c_vocab,c_vector
 
 
-def reconstrut_data(filepath, corppath):
-    x = cPickle.load(open(filepath, "rb"))
-    c_vocab, c_vector = x[0], x[1]
+def reconstrut_data(filepath,corppath):
+    x = cPickle.load(open(filepath,"rb"))
+    c_vocab,c_vector = x[0], x[1]
     c_vector = c_vector.todense()
     tfile = open(corppath, 'w')
     for c in c_vector:
         x = np.squeeze(np.asarray(c))
-        new_string = ''.join([copy_words(c_vocab[i], x[i]) for i in np.flatnonzero(x > 0)])
-        tfile.write("%s\n" % new_string[:-1])
+        new_string = ''.join([copy_words(c_vocab[i],x[i]) for i in np.flatnonzero(x > 0)])
+        tfile.write("%s\n" %new_string[:-1])
     tfile.close()
-
 
 def load_bin_vec(fname, svocab):
     """
     Loads 300x1 word vecs from Google (Mikolov) word2vec
     """
     word_vecs = {}
-    word_vecs["<PAD/>"] = np.zeros(300)
     num_found1 = 0
     with open(fname, "rb") as f:
         header = f.readline()
@@ -163,55 +143,15 @@ def load_bin_vec(fname, svocab):
                     break
                 if ch != '\n':
                     word.append(ch)
-            word = word.decode('utf-8', 'ignore')
+            word = word.decode('utf-8','ignore')
             if word in svocab:
-                num_found1 += 1
+                num_found1+=1
                 word_vecs[word] = np.fromstring(f.read(binary_len), dtype='float32')
                 continue
-            else:
-                f.read(binary_len)
-    print('%d out of %d words found in the word2vec vocab' % (num_found1, len(svocab)))
-    unknown_embed = np.random.uniform(-0.25, 0.25, 300)
-    for i, word in enumerate(svocab):
+            else: f.read(binary_len)
+    print('%d out of %d words found in the word2vec vocab'%(num_found1,len(svocab)))
+    for i,word in enumerate(svocab):
         if word not in word_vecs:
-            print(word)
-            word_vecs[word] = unknown_embed
+            word_vecs[word] = np.random.uniform(-0.1,0.1,300)
 
     return word_vecs
-
-# function for calculating precision and recall for each class
-
-
-def getPrecision(pred_test, yTest, targetLabel):
-    # reverse input argument pred_test yTest to get recall
-    targetLabelCount = 0
-    correctTargetLabelCount = 0
-
-    for idx in range(len(pred_test)):
-        if pred_test[idx] == targetLabel:
-            targetLabelCount += 1
-
-            if pred_test[idx] == yTest[idx]:
-                correctTargetLabelCount += 1
-
-    if correctTargetLabelCount == 0:
-        return 0
-
-    return float(correctTargetLabelCount) / targetLabelCount
-
-
-def getRecall_top3(yTest, pred_test_top3, targetLabel):
-    targetLabelCount = 0
-    correctTargetLabelCount = 0
-
-    for idx in range(len(pred_test)):
-        if pred_test[idx] == targetLabel:
-            targetLabelCount += 1
-
-            if pred_test[idx] == yTest[idx]:
-                correctTargetLabelCount += 1
-
-    if correctTargetLabelCount == 0:
-        return 0
-
-    return float(correctTargetLabelCount) / targetLabelCount
--- API/survey_reader.py
+++ API/survey_reader.py
@@ -9,48 +9,73 @@ import itertools
 from collections import Counter
 from sklearn import preprocessing
 
-
 def convert_label(annot):
-    annot = np.char.lower(annot)
 
     uni_label = np.unique(annot).tolist()
     label = [uni_label.index(x) for x in annot]
-
-    label2idx = {word: idx for idx, word in enumerate(uni_label)}
-
     # uniq_label = []
     # for u in uni_label:
-    #     uniq_label.append(annot.tolist().count(u))
-
-    return [label, Counter(annot.tolist()), label2idx]
+    #     uniq_label.append(annot.tolist().count(u)) Counter(annot.tolist())
 
 
-def read_Surveycsv(filepath, roilist):
+    return [label, annot, uni_label]
+
+def read_Surveycsv(filepath,roilist):
 
     df = pd.read_excel(filepath, usecols=roilist)
     # print(df.describe())
     all_data = np.asarray(df.values.tolist())
-    data = all_data[:, 0]
-    context_label = convert_label(all_data[:, 1])
-    content_label = convert_label(all_data[:, 2])
-    driver_label = convert_label(all_data[:, 3])
+    data = all_data[:,0]
+    context_label = convert_label(all_data[:,1]) 
+    content_label = convert_label(all_data[:,2]) 
+    driver_label = convert_label(all_data[:,3])
 
-    return [data, [context_label, content_label, driver_label]]
-
+    return [data,[context_label,content_label,driver_label]]
 
 def fileWriter(filename, file):
     """
     author: Pengfei
     date: 07/06/2017
     """
-    thefile = open('C:/Users/pli006/Documents/Sourcetree/review_cluster/data/' + filename, 'w')
+    thefile = open('' + filename, 'w')
     for sentence in file:
         thefile.write("%s\n" % sentence)
     thefile.close()
 
+def write_Label(content, listuple, csv_path, index):
+    ##creating pandas
+    idx_comment = range(len(content))
+    df_comment = pd.DataFrame({'id': idx_comment, 'comment': content})
+    df_survey = pd.DataFrame(listuple, columns=['sentence', 'id', 'label'])
+    df_all = pd.merge(df_comment, df_survey, on='id', how='inner')
+    df_final = df_all.groupby(['id', 'comment'])['label'].apply(list).to_frame()
+
+    df_final['label'] = df_final['label'].apply(lambda x: rule_all(x))
+
+    ### Write label to original CSV file
+    with open(csv_path, 'r') as csvinput:
+        reader = csv.reader(csvinput)
+        all = []
+        row = next(reader)
+        if len(row) == 36:
+            row.append('Class_Label')
+        all.append(row)
+
+        count = 0
+        for i, row in enumerate(reader):
+            if i in index and len(row) == 36:
+                row.append(df_final['label'].values[count])
+                count += 1
+            all.append(row)
+    with open(csv_path, 'w') as csvoutput:
+        writer = csv.writer(csvoutput, lineterminator='\n')
+        writer.writerows(all)
 
 if __name__ == '__main__':
 
     xlsx_file = '/home/dongzhe/Documents/review_cluster/data/HT Data.xlsx'
-    roi_list = ['HT Experience', 'Context', 'Content', 'Driver']
-    data, label = read_Surveycsv(xlsx_file, roi_list)
+    roi_list = ['HT Experience','Context','Content','Driver']
+    data, label = read_Surveycsv(xlsx_file,roi_list)
+
+
+
